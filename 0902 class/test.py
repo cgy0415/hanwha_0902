49b5a201bdae38f71f28ed4dd2b84e226327f50c
cgy0415 <rosachoi97@gmail.com>
2026-09-08 15:31:42 +0900

    Create test.py

---
+++ 0902 class/test.py
@@ -0,0 +1,47 @@
+#print("AI 서비스 백엔드 프로그래밍 실무") #기본
+# print("=============================")
+# print("파이썬 기본 문법, 시간:8")
+# print("클래스, 시간:8")
+# print("데코레이터, 시간:8")
+# print("예외 처리, 시간:8")
+# print("로깅, 시간:8")
+
+# print("AI 서비스 백엔드 프로그래밍 실무") #변수활용버전
+# print("=============================")
+# x = "시간:8"
+# print(f"파이썬 기본 문법, {x}")
+# print(f"클래스, {x}")
+# print(f"데코레이터, {x}")
+# print(f"예외 처리, {x}")
+# print(f"로깅, {x}")
+
+# title = "AI 서비스 백엔드 프로그래밍 실무" #선생님 예제버전
+# line = "============="
+# time = 8
+# a = "파이썬 기본 문법"
+# print(title)
+# print(line)
+# print(a, time, sep=", 시간:")
+
+#리스트 이용
+# title = "AI 서비스 백엔드 프로그래밍 실무" 
+# line = "=============="
+# time = 8
+# myList = ["파이썬 기본 문법", "클래스", "데코레이터", "예외 처리", "로깅"]
+# print(title)
+# print(line)
+# print(myList[0], time, sep=", 시간:") 
+# print(myList[1], time, sep=", 시간:")
+# print(myList[2], time, sep=", 시간:")
+# print(myList[3], time, sep=", 시간:")
+# print(myList[4], time, sep=", 시간:")
+
+#for in 문 활용, for문 마지막에는 : 넣고 밑에 칸에 tab
+# title = "AI 서비스 백엔드 프로그래밍 실무" 
+# line = "=============="
+# time = 8
+# myList = ["파이썬 기본 문법", "클래스", "데코레이터", "예외 처리", "로깅"]
+# print(title)
+# print(line)
+# for x in myList:
+#     print(x, time, sep=", 시간:")
